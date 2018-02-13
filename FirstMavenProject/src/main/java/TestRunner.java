f7c70b5f58a2c1d82b179a9211a9bed124aa1cec
Aditya Gaur <adityagaur.2411@gmail.com>
2018-02-13 21:27:38 +0530

    updated test runner

--- FirstMavenProject/src/main/java/TestRunner.java
+++ FirstMavenProject/src/main/java/TestRunner.java
@@ -1,10 +1,13 @@
+//import org.junit.runner.RunWith;
+
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src//test//resources", glue="com.test.java", tags={"@tag1"})
+@CucumberOptions(features="src//test//resources", glue="com.test.java")
 public class TestRunner {
 
 }
